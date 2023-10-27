86156994092250ae2d637bf8538a8d71bcf3f12d
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-10-27 10:43:09 -0700

    fixed home page layout, added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About LuxEstate</h1>
+      <p className='mb-4 text-slate-700'>LuxEstate is an innovative Real Estate technology solution that specializes in helping clients buy, sell, and rent properties directly with the property owner.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our users achieve their real estate goals by providing the tools they need. LuxEstate allows sellers to list their homes for free and get their listings seen by potential buyers. It also allows buyers to browse homes and contact the seller directly with no middleman required. 
+      </p>
+      <p className='mb-4 text-slate-700'>We strive to empower buyers and sellers by enabling them to save on Realtor fees. We encourage direct transactions with licensed brokers, eliminating the need for traditional real estate agents and their associated costs.</p> 
+      {/* <img src="" alt="LuxEstate Image" className="mt-4" /> */}
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -86,9 +86,9 @@ export default function Home() {
           ))}
       </Swiper>
 
-      {/* listing results for offer, sale and rent */}
+      {/* listing results for offer, sale and rent, *changed 6xl to 8xl and added items-center to div below */}
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+      <div className='max-w-8xl items-center mx-auto p-3 flex flex-col gap-8 my-10'>
         {offerListings && offerListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
